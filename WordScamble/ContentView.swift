ab9e83e7d704a34914420e6ed9ef838e55c17279
andydangit <andy.dang.it@gmail.com>
2024-10-08 05:27:00 -0500

    Add: ntf16 and UTTextChecker

--- WordScamble/ContentView.swift
+++ WordScamble/ContentView.swift
@@ -26,13 +26,37 @@ struct ContentView: View {
     }
     
     
-    func testBundle() {
-        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL, encoding: .utf8) {
-                // we loaded the file into a string!
-            }
-        }
+    //    func testBundle() {
+    //        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
+    //            if let fileContents = try? String(contentsOf: fileURL, encoding: .utf8) {
+    //                // we loaded the file into a string!
+    //            }
+    //        }
+    //    }
+    
+    
+    func testSTring() {
+        
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
+        
+        
+//        let input = """
+//        a   
+//        b
+//        c
+//        """
+//        let letters = input.components(separatedBy: "\n")
+//        let letter = letters.randomElement()
+//        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
     }
+    
+    
     
 }
 
