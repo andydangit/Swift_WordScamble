c855205453bd97c8032b508dac94e532d4394fb3
andydangit <andy.dang.it@gmail.com>
2024-10-08 04:49:11 -0500

    Initial Commit

---
+++ WordScamble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordScamble
+//
+//  Created by Andy Dang It on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScamble/WordScambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScambleApp.swift
+//  WordScamble
+//
+//  Created by Andy Dang It on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
