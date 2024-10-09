601ac8741f2bb2f66cbabacac5a837113438b896
andydangit <andy.dang.it@gmail.com>
2024-10-09 05:24:11 -0500

    Add: Animation and textField

--- WordScamble/ContentView.swift
+++ WordScamble/ContentView.swift
@@ -9,52 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let people = ["Andy", "Dang", "It"]
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     
     var body: some View {
-        
-        List{
-            Text("Static Row")
-            
-            //            Dynamic row
-            ForEach(people, id: \.self) {
-                Text($0)
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section{
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
             }
-            
-            Text("Static Row")
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
         }
+        
     }
     
     
-    //    func testBundle() {
-    //        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
-    //            if let fileContents = try? String(contentsOf: fileURL, encoding: .utf8) {
-    //                // we loaded the file into a string!
-    //            }
-    //        }
-    //    }
-    
-    
-    func testSTring() {
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        let word = "swift"
-        let checker = UITextChecker()
+        guard answer.count > 0 else { return}
         
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        
-        let allGood = misspelledRange.location == NSNotFound
-        
-        
-//        let input = """
-//        a   
-//        b
-//        c
-//        """
-//        let letters = input.components(separatedBy: "\n")
-//        let letter = letters.randomElement()
-//        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        withAnimation{
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
     }
+    
+    
+        
+        
+
     
     
     
